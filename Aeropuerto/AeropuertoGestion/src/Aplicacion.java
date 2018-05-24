d08b3791cadc8828b5c977ccf39a6c508ea5ab65
gabrielespina <gabriel.espina@comunidad.ub.edu.ar>
2018-05-24 18:22:03 -0300

    Se agrega la clase y package simulacion de aeropuerto.
    Se logra hacer un mini menu para abm con los datos a modificar ya
    indexados en la clase Menu.java y los gestiona una version de aeropuerto
    server simulado

--- Aeropuerto/AeropuertoGestion/src/Aplicacion.java
+++ Aeropuerto/AeropuertoGestion/src/Aplicacion.java
@@ -1,12 +1,29 @@
 import ar.edu.ub.p3.aeropuerto.conexion.*;
 import ar.edu.ub.p3.aeropuerto.configuracion.Configuracion;
+import ar.edu.ub.p3.aeropuerto.menu.consola.Menu;
+import ar.edu.ub.p3.interfaz.IAvion;
 
 public class Aplicacion {
 
+	//JUST - STRING IT - 
 	public static void main(String[] args) {
 		
 		ConexionAeropuerto conexion = new ConexionAeropuertoSimulada(new Configuracion("appgestion.properties"));
-	
+		
+		
+		Menu menu = new Menu();
+		
+		for(IAvion avion : conexion.getAviones())
+			System.out.println(avion.toString());
+		
+		menu.menuPrincipal(conexion);
+		
+		for(IAvion avion : conexion.getAviones())
+			System.out.println(avion.toString());
+		
+		
+		
+		
 		//CREAR LA CARGA FANTASMA DE DATOS - ARCHIVO CON CARGA 
 		//CREAR EL MENU PARA OPCIONES ABM
 		//CREAR EL METODO PARA MODIFICAR EL AVION
@@ -19,31 +36,7 @@ public class Aplicacion {
 		
 		//CONECTAR Y DESCONECTAR QUEDA PARA LO ULTIMO
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		//esto es codigo viejo no se si sirve aun, por las dudis lo dejo aca ----------------no tocar-------stringforlife---------------
 		
 		/*
 		ConfiguracionAppGestion configuracion = new ConfiguracionAppGestion ( args.length > 0 ? args[0] : "appgestion.properties" );
--- Aeropuerto/AeropuertoGestion/src/ar/edu/ub/p3/aeropuerto/conexion/ConexionAeropuertoSimulada.java
+++ Aeropuerto/AeropuertoGestion/src/ar/edu/ub/p3/aeropuerto/conexion/ConexionAeropuertoSimulada.java
@@ -2,6 +2,7 @@ package ar.edu.ub.p3.aeropuerto.conexion;
 
 import java.util.List;
 
+import ar.edu.ub.p3.aeropuerto.conexion.simulada.AeropuertoServidorSimulado;
 import ar.edu.ub.p3.aeropuerto.configuracion.Configuracion;
 import ar.edu.ub.p3.interfaz.IAeropuerto;
 import ar.edu.ub.p3.interfaz.IAvion;
@@ -9,9 +10,12 @@ import ar.edu.ub.p3.interfaz.IVuelo;
 
 public class ConexionAeropuertoSimulada extends ConexionAeropuerto{
 
+	private AeropuertoServidorSimulado servidorAeropuerto;
+	
+	
 	public ConexionAeropuertoSimulada(Configuracion configuracion) {
 		super(configuracion);
-		// TODO Auto-generated constructor stub
+		setServidorAeropuerto(new AeropuertoServidorSimulado());
 	}
 
 	//---------------------------------------------------------------------------------------
@@ -54,9 +58,9 @@ public class ConexionAeropuertoSimulada extends ConexionAeropuerto{
 
 	@Override//MODIFICAR ESTE METODO........................
 	public List<IAvion> getAviones() {
-		//LE PIDO LA LISTA DE  IAVIONES AL SERVER
-		//CARGAR UNA LISTA SIMULADA
-		return null;
+		
+		return getServidorAeropuerto().getAviones();
+		
 	}
 	//---------------------------------------------------------------------------------------
 	//OPERACIONES DE AVIONES-----------------------------------------------------------------
@@ -65,22 +69,24 @@ public class ConexionAeropuertoSimulada extends ConexionAeropuerto{
 	@Override//MODIFICAR ESTE METODO........................
 	public void addAvion(IAvion avion) {
 		
-		//SE ENVIA AL SERVER LA INTERFAZ DEL AVION NUEVO
-		//SIMULAR EL ENVIO DE EL IAVION NUEVO
+		
+		getServidorAeropuerto().addAvion(avion);
+		
 	}
 
 	@Override//MODIFICAR ESTE METODO........................
 	public void modifyAvion(IAvion avion) {
+		//SE DEBERIA CREAR EL MENSAJE DE MODIFICACION PERO SE SIMULA QUE ESTAMOS YA CONECTADOS
+		getServidorAeropuerto().modifyAvion(avion);
 		
-		//SE ENVIA AL SERVER LA INTERFZA DEL AVION MODIFICADO 
-		//SIMULAR EL ENVIO DE UN IAVION MODIFICADO
 	}
 
 	@Override//MODIFICAR ESTE METODO........................
 	public void deleteAvion(IAvion avion) {
 		
-		//SE ENVIA AL SERVER EL IAVION QUE SE VA ELIMINAR 
-		//SIMULAR EL ENVIO PARA LA BAJA DE UN IAVION
+		//SIMULO LA ORDEN PARA QUE EL SERVIDOR ELIMINE ESE AVION
+		getServidorAeropuerto().deleteAvion(avion);
+		
 	}
 	//---------------------------------------------------------------------------------------
 	//OPERACIONES DE VUELOS------------------------------------------------------------------
@@ -102,6 +108,14 @@ public class ConexionAeropuertoSimulada extends ConexionAeropuerto{
 	public void deleteVuelo(IVuelo vuelo) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public AeropuertoServidorSimulado getServidorAeropuerto() {
+		return servidorAeropuerto;
+	}
+
+	public void setServidorAeropuerto(AeropuertoServidorSimulado aeropuerto) {
+		this.servidorAeropuerto = aeropuerto;
 	}
 
 	
