57238e512851f3b077b63f0a30b91e846dac4e54
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-05-27 11:43:48 -0300

    Se agrega la clase ConexionAeropuerto

---
+++ Aeropuerto/AeropuertoTableroLlegadas/src/ar/edu/ub/p3/aeropuerto/conexion/ConexionAeropuerto.java
@@ -0,0 +1,34 @@
+package ar.edu.ub.p3.aeropuerto.conexion;
+
+import java.util.List;
+
+import ar.edu.ub.p3.interfaz.IAeropuerto;
+import ar.edu.ub.p3.interfaz.IAvion;
+import ar.edu.ub.p3.interfaz.IVuelo;
+import ar.edu.ub.p3.aeropuerto.configuracion.Configuracion;
+
+public abstract class ConexionAeropuerto {
+	public ConexionAeropuerto( Configuracion configuracion ) {
+		
+	}
+	
+	//Operaciones de conectividad
+	public abstract void              conectarAlAeropuerto();
+	public abstract void              desconectarDelAeropuerto();
+	
+	//Operaciones de lista
+	public abstract List<IAeropuerto> getAeropuertos();
+	public abstract List<IVuelo>      getVuelos();
+	public abstract List<IVuelo>      getVuelosCercanosAlAeropuerto();	
+	public abstract List<IAvion>      getAviones();	
+	
+	//Operaciones de aviones
+	public abstract void              addAvion( IAvion avion );
+	public abstract void              modifyAvion( IAvion avion );
+	public abstract void              deleteAvion( IAvion avion );
+	
+	//Operaciones de vuelo
+	public abstract void              addVuelo( IVuelo vuelo );
+	public abstract void              modifyVuelo( IVuelo vuelo );
+	public abstract void              deleteVuelo( IVuelo vuelo );	
+}
