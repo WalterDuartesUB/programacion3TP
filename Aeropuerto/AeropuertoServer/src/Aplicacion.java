bf5cf7a24a0caca78361947e4502d7e8f0625273
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-05-29 12:46:25 -0300

    Aparece la ventana principal del Servidor de Aeropuerto

--- Aeropuerto/AeropuertoServer/src/Aplicacion.java
+++ Aeropuerto/AeropuertoServer/src/Aplicacion.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 import ar.edu.ub.p3.conexion.ConexionTraficoAereo;
 import ar.edu.ub.p3.modelo.EstadoAeropuerto;
 import ar.edu.ub.p3.util.Configuracion;
+import ar.edu.ub.p3.vista.VentanaPrincipal;
 
 public class Aplicacion {
 
 	public static void main(String[] args) {
+		
+		new VentanaPrincipal();
+		
 		Scanner in = new Scanner( System.in );
 		Configuracion configuracion = new Configuracion(args[0]);
 		EstadoAeropuerto estadoAeropuerto = new EstadoAeropuerto(configuracion);
