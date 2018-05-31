cc47f4c1c0b9bd6710b9946c805223fc0757f0e6
Fernando Romero <fromero@HCJSM.local>
2018-05-31 10:44:28 -0300

    se modifica la funcion agregarTabla

--- Aeropuerto/AeropuertoTableroLlegadas/src/ar/edu/ub/p3/aeropuerto/tablero/view/Ventana.java
+++ Aeropuerto/AeropuertoTableroLlegadas/src/ar/edu/ub/p3/aeropuerto/tablero/view/Ventana.java
@@ -1,6 +1,7 @@
 package ar.edu.ub.p3.aeropuerto.tablero.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,6 +9,7 @@ import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
@@ -22,17 +24,7 @@ public class Ventana extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
-	private JLabel punto1x;           // etiqueta o texto no editable
-    private JLabel punto1y;
-    private JLabel punto2x;
-    private JLabel punto2y;
 
-    private JButton boton;          // boton con una determinada accion
-
-
-	
     public Ventana(ConexionAeropuerto conexion) {
         super();                    // usamos el contructor de la clase padre JFrame
         configurarVentana();        // configuramos la ventana
@@ -48,19 +40,34 @@ public class Ventana extends JFrame {
         this.setLayout(null);                                   // no usamos ningun layout, solo asi podremos dar posiciones a los componentes
         this.setResizable(false);                               // hacemos que la ventana no sea redimiensionable
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // hacemos que cuando se cierre la ventana termina todo proceso
+        this.getContentPane().setLayout(null);
 
     }
-
- 
+  
 
     private void agregarTabla(ConexionAeropuerto conexion) {
 
     	String[] 	nombreColumna = {"NroVuelo", "Origen", "Destino", "Aerolinea", "Hora Programada"};
     
-    	// arreglar esto aalgun momento que se redimensione segun cantidad de vuelos
     	Object [][] aux = new Object [3][5];
     	
+    	Object [][] aux3 = new Object [conexion.getVuelos().size()][nombreColumna.length];
+    	
     	Object [][] aux2 = {
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
+    						{"pepe","pepe","pepe","pepe","pepe"},
     						{"pepe","pepe","pepe","pepe","pepe"},
     						{"pepe","pepe","pepe","pepe","pepe"},
     						{"pepe","pepe","pepe","pepe","pepe"},
@@ -101,25 +108,46 @@ public class Ventana extends JFrame {
     	};
     	
     	
+    	
     	int i = 0;
     	
 		for(IVuelo vuelo : conexion.getVuelos()) {
-			aux[i][0] = (vuelo.getIdVuelo().toString());
-			aux[i][3] = (vuelo.getAvion().getAerolinea().toString());
-			aux[i][2] = ( vuelo.getAeropuertoDestino().getIdAeropuerto() + " - "+vuelo.getAeropuertoDestino().getNombre());
-			aux[i][1] = ( vuelo.getAeropuertoOrigen().getIdAeropuerto() +" - "+ vuelo.getAeropuertoOrigen().getNombre());
-			aux[i][4] = ( vuelo.getHorarioProgramado().getHours() +":"+vuelo.getHorarioProgramado().getMinutes());
+			aux3[i][0] = (vuelo.getIdVuelo().toString());
+			aux3[i][3] = (vuelo.getAvion().getAerolinea().toString());
+			aux3[i][2] = ( vuelo.getAeropuertoDestino().getIdAeropuerto() + " - "+vuelo.getAeropuertoDestino().getNombre());
+			aux3[i][1] = ( vuelo.getAeropuertoOrigen().getIdAeropuerto() +" - "+ vuelo.getAeropuertoOrigen().getNombre());
+			aux3[i][4] = ( vuelo.getHorarioProgramado().getHours() +":"+vuelo.getHorarioProgramado().getMinutes());
 			
 			i++;
 		}
     	
 
-    	JTable tabla = new JTable(aux,nombreColumna);
+    	JTable tabla = new JTable(aux2,nombreColumna);
+    	JScrollPane scroll =  new JScrollPane(tabla, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    	scroll.setBackground(Color.DARK_GRAY);
+    	
+    	tabla.setAutoscrolls(true);
+ 
     	
     	tabla.setSize(200,200);
+    	
+    	getContentPane().setLayout(new BorderLayout());
+    	getContentPane().add(tabla.getTableHeader(), BorderLayout.PAGE_START);
+    	getContentPane().add(scroll,BorderLayout.CENTER);
+    	
+    	JPanel botones = new JPanel();
+    	scroll.setRowHeaderView(botones);
+    	botones.setLayout(null);
+
     	tabla.setVisible(true);
     	tabla.setEnabled(false);
+    	tabla.setAutoscrolls(true);
 
+
+        tabla.setForeground(Color.BLUE);
+        
+        tabla.setGridColor(Color.RED);
+        tabla.setSelectionBackground(Color.YELLOW);
     	
     	this.setLayout(new BorderLayout());
     	this.add(tabla.getTableHeader(), BorderLayout.PAGE_START);
