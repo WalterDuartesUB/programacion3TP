b0c71b7859badaa41ef558d3abc93e445c03c194
fernandoromero91 <37422646+fernandoromero91@users.noreply.github.com>
2018-06-17 14:17:14 -0300

    Se avanza con el tablero de Salidas

--- Aeropuerto/AeropuertoServer/src/ar/edu/ub/p3/vista/PanelDespegue.java
+++ Aeropuerto/AeropuertoServer/src/ar/edu/ub/p3/vista/PanelDespegue.java
@@ -3,13 +3,18 @@ package ar.edu.ub.p3.vista;
 
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -21,135 +26,80 @@ import javax.swing.Timer;
 
 
 import ar.edu.ub.p3.conexion.ConexionTraficoAereo;
+import ar.edu.ub.p3.interfaz.IPosicion;
 import ar.edu.ub.p3.interfaz.IVuelo;
 import ar.edu.ub.p3.modelo.Aeropuerto;
 import ar.edu.ub.p3.modelo.Vuelo;
 import ar.edu.ub.p3.util.Configuracion;
 
-public class PanelDespegue extends JPanel {
+public class PanelDespegue extends JPanel implements Closeable{
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	private Collection<String> 		idVuelos;
-	private String					miIdAeropuerto;
+	
+	
 	private ConexionTraficoAereo 	conexionTA;
 	private Configuracion 			configuracion;
+	private String					miIdAeropuerto;
+	private Collection<Vuelo> 		vuelosProgramadosDespegue;
+	private Collection<Vuelo>		vuelosPorAterrizar;
+	private Timer 					timerPedirVuelos;
 	
+
 
 	public PanelDespegue(Configuracion configuracion, String idAeropuerto, ConexionTraficoAereo conexionTA) {
+		super();
 		this.setConexionTA(conexionTA);
 		this.setMiIdAeropuerto(idAeropuerto);
+		this.setVuelosProgramadosDespegue(new LinkedList<Vuelo>());
+		this.setVuelosPorAterrizar(new LinkedList<>());
 		this.setConfiguracion(configuracion);
 		
-		this.setVuelos( new LinkedList<String>() );
-		
-
-		//Timer para pedir la posicion de los aviones al trafico aereo
-		new Timer( 1000, this::pedirVuelosAlTraficoAereo).start();
+		this.setTimerPedirVuelos(new Timer (this.getConfiguracion().getConfiguracionAsInt("tableroTiempoRefresh"), this ::pedirVuelosAlTraficoAereo));
+		this.getTimerPedirVuelos().start();
 	}
 	
-	public void pedirVuelosAlTraficoAereo(ActionEvent arg0) {	
-			
-		//Preguntar donde saco los vuelos
-		//this.setVuelos(this.getConexionTA().obtenerInformacionVuelos(this.getVuelos()));
-		this.dibujarTableroDespegue();
-	}
 
-	private void dibujarTableroDespegue() {
-		this.validate();
-		this.repaint();
+	public void pedirVuelosAlTraficoAereo(ActionEvent arg0) {
+		this.setVuelosProgramadosDespegue(this.getConexionTA().getEstadoAeropuerto().getVuelosProximoADespegarYDespegados().values());
+		this.dibujarSalidas();
 	}
 	
-	private void iniciarVentana() {
-     	
-		JPanel panel = new JPanel(new GridBagLayout());repaint();
-		//this.getContentPane().add(panel);
-		this.add(panel);
+	  public void Iniciar() {
+	    int width = getWidth();
+	    int height = getHeight();
+	    int anchoAlto = Integer.min(width, height);
+	    this.add(dibujarTabla());
+	    System.out.println("hola");
+	    
+	    
+	}
+	
+	private Component dibujarTabla() {
 		
+		JPanel panel = new JPanel(new GridBagLayout());
 		
 		String[] 	nombreColumna = {"Hora Programada","NroVuelo", "Aerolinea", "Destino", "Estima", "Estado"};
+		Object [][] aux2 = {
+				{"pepe","pepe","pepe","pepe","pepe","pepe"},
+				{"pepe","pepe","pepe","pepe","pepe","pepe"},
+				{"pepe","pepe","pepe","pepe","pepe","pepe"},
+				{"pepe","pepe","pepe","pepe","pepe","pepe"},
+};
 		
-		// acomodar segun tablero llegada o salida
-    	Object [][] vuelos = new Object [conexionTA.getVuelosProximoDespegue().size()][nombreColumna.length];
-   
-
-    	int i = 0;
+    	JTable tabla = new JTable(aux2,nombreColumna);
+    	JScrollPane scroll =  new JScrollPane(tabla, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    	scroll.setBackground(Color.DARK_GRAY);
     	
-    	ImageIcon icon1 = new ImageIcon("C:\\WorkSpaceIntegrador\\programacion3TP\\img\\arg2.png");
-    	ImageIcon icon2 = new ImageIcon("C:\\WorkSpaceIntegrador\\programacion3TP\\img\\latam2.png");
-    	ImageIcon icon3 = new ImageIcon("C:\\WorkSpaceIntegrador\\programacion3TP\\img\\andes2.png");
-		
+    	tabla.setAutoscrolls(true);
+ 
     	
-		for(IVuelo vuelo : conexionTA.getVuelosProximoDespegue()) {
-    
-			
-			if(estoyEnMiAeropuerto(vuelo,miIdAeropuerto)) {
-
-				vuelos[i][0] = ( vuelo.getHorarioProgramado().getHours() +":"+vuelo.getHorarioProgramado().getMinutes());  // arreglar en algun momento
-				vuelos[i][1] = (vuelo.getIdVuelo().toString());
-				
-				///// HACER ESTO ESCALABLE  solo prueba
-				if(vuelo.getAvion().getAerolinea().getNombre().equals("AEROLINEAS ARGENTINAS"))
-					vuelos[i][2] = icon1;
-				if(vuelo.getAvion().getAerolinea().getNombre().equals("LATAM"))
-					vuelos[i][2] = icon2;
-				if(vuelo.getAvion().getAerolinea().getNombre().equals("ANDES"))
-					vuelos[i][2] = icon3;
-				vuelos[i][3] = ( vuelo.getAeropuertoDestino().getIdAeropuerto() + " - "+vuelo.getAeropuertoDestino().getNombre());
-				vuelos[i][4] = (vuelo.getHorarioDespegue().getHours() + ":" + vuelo.getHorarioDespegue().getMinutes());
-				vuelos[i][5] = (vuelo.getEstadoVuelo().toString());
-				i++;
-			}
-		}
-		
-
-		
-        JTable tabla = new JTable(vuelos,nombreColumna) {
-            @Override
-            public Class<?> getColumnClass(int column) {
-                switch (column) {
-                case 2 : return ImageIcon.class;
-                	default: return Object.class;
-                }
-            }
-        };
-       
-		tabla.setRowHeight(50);
-		tabla.setEnabled(false);
-		tabla.getColumnModel().getColumn(2).setPreferredWidth(150);
-		
-		
+    	tabla.setSize(200,200);
+    	
 		JScrollPane tableScrollPane = new JScrollPane(tabla);
 		tableScrollPane.setPreferredSize(new Dimension(400, 400));
-		
-		
-		
-		
-		JLabel jLabelObject = new JLabel();
-		jLabelObject.setIcon(new ImageIcon("C:\\WorkSpaceIntegrador\\programacion3TP\\img\\salidas.png"));
-		
-	
-		String date = new Date().toString();
-		JLabel dia = new JLabel(date);
-		
-		JLabel aeropuertoactuall = new JLabel(miIdAeropuerto);
-		
-		
-
-		JPanel detailsPanel = new JPanel();
-		detailsPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-		
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.anchor = GridBagConstraints.WEST;
-		panel.add(jLabelObject, gbc);
-		
-
+    	
+    	GridBagConstraints gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.CENTER;
 		gbc.gridx = 0;
 		gbc.gridy = 2;		
@@ -157,48 +107,17 @@ public class PanelDespegue extends JPanel {
 		gbc.weightx = 2.0;
 		gbc.weighty = 2.0;
 		panel.add(tableScrollPane, gbc);
-
-		gbc.fill = GridBagConstraints.NONE;
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.weightx = 0;
-		gbc.weighty = 0;
-		panel.add(dia, gbc);
+    	
+    	return tabla;
+    	
 		
-		gbc.fill = GridBagConstraints.NONE;
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		gbc.weightx = 0;
-		gbc.weighty = 0;
-		panel.add(aeropuertoactuall, gbc);
+	}
 
-		//this.pack();
+
+	private void dibujarSalidas() {
+		this.validate();
+		this.Iniciar();
 		
-		
-		//this.setVisible(true);
-    }
-
-	private boolean estoyEnMiAeropuerto(IVuelo vuelo, String miIdAeropuerto2) {
-		return vuelo.getAeropuertoOrigen().getIdAeropuerto().equals(miIdAeropuerto2);
-	}
-
-	public Collection<String> getVuelos() {
-		return idVuelos;
-	}
-
-
-	private void setVuelos(Collection<String> vuelos) {
-		this.idVuelos = vuelos;
-	}
-
-
-	public String getMiIdAeropuerto() {
-		return miIdAeropuerto;
-	}
-
-
-	private void setMiIdAeropuerto(String miIdAeropuerto) {
-		this.miIdAeropuerto = miIdAeropuerto;
 	}
 
 
@@ -207,9 +126,11 @@ public class PanelDespegue extends JPanel {
 	}
 
 
-	public void setConexionTA(ConexionTraficoAereo conexionTA) {
+
+	private void setConexionTA(ConexionTraficoAereo conexionTA) {
 		this.conexionTA = conexionTA;
 	}
+
 
 
 	public Configuracion getConfiguracion() {
@@ -217,9 +138,67 @@ public class PanelDespegue extends JPanel {
 	}
 
 
+
 	private void setConfiguracion(Configuracion configuracion) {
 		this.configuracion = configuracion;
 	}
+
+
+
+	public String getMiIdAeropuerto() {
+		return miIdAeropuerto;
+	}
+
+
+
+	private void setMiIdAeropuerto(String miIdAeropuerto) {
+		this.miIdAeropuerto = miIdAeropuerto;
+	}
+
+
+
+	public Collection<Vuelo> getVuelosProgramadosDespegue() {
+		return vuelosProgramadosDespegue;
+	}
+
+
+
+	private void setVuelosProgramadosDespegue(Collection<Vuelo> vuelosProgramadosDespegue) {
+		this.vuelosProgramadosDespegue = vuelosProgramadosDespegue;
+	}
+
+
+
+	public Collection<Vuelo> getVuelosPorAterrizar() {
+		return vuelosPorAterrizar;
+	}
+
+
+
+	private void setVuelosPorAterrizar(Collection<Vuelo> vuelosPorAterrizar) {
+		this.vuelosPorAterrizar = vuelosPorAterrizar;
+	}
+
+
+
+	public Timer getTimerPedirVuelos() {
+		return timerPedirVuelos;
+	}
+
+
+
+	private void setTimerPedirVuelos(Timer timerPedirVuelos) {
+		this.timerPedirVuelos = timerPedirVuelos;
+	}
+
+
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 
 
--- Aeropuerto/AeropuertoServer/src/ar/edu/ub/p3/vista/VentanaDespegue.java
+++ Aeropuerto/AeropuertoServer/src/ar/edu/ub/p3/vista/VentanaDespegue.java
@@ -1,15 +1,21 @@
 package ar.edu.ub.p3.vista;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.Date;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 
 import ar.edu.ub.p3.conexion.ConexionTraficoAereo;
 import ar.edu.ub.p3.interfaz.IAeropuerto;
-import ar.edu.ub.p3.modelo.Aeropuerto;
 import ar.edu.ub.p3.util.Configuracion;
 
-public class VentanaDespegue extends JDialog {
+public class VentanaDespegue extends JDialog implements WindowListener {
 
 
 
@@ -19,28 +25,75 @@ public class VentanaDespegue extends JDialog {
 	private static final long serialVersionUID = -7253803356902721825L;
 	
 	
-	private IAeropuerto aerpuerto;
+	private IAeropuerto aeropuerto;
 	private ConexionTraficoAereo conexionTA;
 	private Configuracion configuracion;
+	private PanelDespegue panelDespegue;
 
-	public VentanaDespegue(Configuracion configuracion, Aeropuerto aerpuerto, ConexionTraficoAereo conexionTA) {
+	public VentanaDespegue(Configuracion configuracion, IAeropuerto aeropuerto, ConexionTraficoAereo conexionTA) {
 		
-		this.setAerpuerto(aerpuerto);
+		this.setAeropuerto(aeropuerto);
 		this.setConexionTA(conexionTA);
 		this.setConfiguracion(configuracion);
 		
 		this.setTitle("Despegues");
-		this.setResizable(false);
-		this.add( new PanelDespegue( this.getConfiguracion(), this.getAerpuerto().getIdAeropuerto(), this.getConexionTA() ),BorderLayout.CENTER);
+		this.setLayout(new GridBagLayout());
+		this.setSize(400,400);
+		this.setPanelDespegue( new PanelDespegue( this.getConfiguracion(), this.getAeropuerto().getIdAeropuerto(), this.getConexionTA() ));
+		//this.add(this.getPanelDespegue(),BorderLayout.CENTER);
+		this.addWindowListener(this);
 		this.setVisible(true);	
+		
+		
+		
+		JLabel jLabelObject = new JLabel();
+		jLabelObject.setIcon(new ImageIcon("./encabezadotablero/salidas.png"));
+		//"./mapas/"
+		
+		String date = new Date().toString();
+		JLabel dia = new JLabel(date);
+		
+		//String aeropuertoactual = aeropuertolocal.getIdAeropuerto().toString()+"-"+aeropuertolocal.getNombre();
+		String aeropuertoactual= this.getAeropuerto().getIdAeropuerto().toString()+"-"+this.getAeropuerto().getNombre().toString();
+		JLabel aeropuertoactuall = new JLabel(aeropuertoactual);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.WEST;
+		this.add(jLabelObject, gbc);
+		
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		this.add(dia, gbc);
+		
+		
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.weightx = 0;
+		gbc.weighty = 0;
+		this.add(aeropuertoactuall, gbc);
+		
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.gridx = 0;
+		gbc.gridy = 2;		
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 2.0;
+		gbc.weighty = 2.0;
+		this.add(this.getPanelDespegue(), gbc);
+	}
+	
+	public IAeropuerto getAeropuerto() {
+		return aeropuerto;
 	}
 
-	public IAeropuerto getAerpuerto() {
-		return aerpuerto;
-	}
-
-	private void setAerpuerto(IAeropuerto aerpuerto) {
-		this.aerpuerto = aerpuerto;
+	private void setAeropuerto(IAeropuerto aeropuerto) {
+		this.aeropuerto = aeropuerto;
 	}
 
 	public ConexionTraficoAereo getConexionTA() {
@@ -58,5 +111,57 @@ public class VentanaDespegue extends JDialog {
 	private void setConfiguracion(Configuracion configuracion) {
 		this.configuracion = configuracion;
 	}
+
+	public PanelDespegue getPanelDespegue() {
+		return panelDespegue;
+	}
+
+	private void setPanelDespegue(PanelDespegue panelDespegue) {
+		this.panelDespegue = panelDespegue;
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
